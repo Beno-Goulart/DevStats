9aaecfeebf6f76a414a90e14a77d859ee9d2a482
Beno-Goulart <benocampos18@gmail.com>
2026-07-12 13:36:12 -0300

    fix: wrap image field value in object for Discord API

--- DevStats-Backend/src/main/java/devstats/models/ImageField.java
+++ DevStats-Backend/src/main/java/devstats/models/ImageField.java
@@ -1,12 +1,14 @@
 package devstats.models;
 
+import java.util.Map;
+
 public class ImageField extends DynamicField {
 
     public ImageField() {
     }
 
-    public ImageField(int type, String name, String value) {
-        super(type, name, value);
+    public ImageField(int type, String name, String url) {
+        super(type, name, Map.of("url", url));
     }
 
 }
